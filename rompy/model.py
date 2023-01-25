54116acbe630729ca20b9beaa9173a78cf4c6fe8
Rafael Guedes <r.guedes@oceanum.science>
2023-01-25 13:36:19 +1300

    Add initial draft

---
+++ rompy/model.py
@@ -0,0 +1,71 @@
+"""Base model wrapper.
+
+Principles
+----------
+pydantic
+    - BaseModel with useful capability
+    - Type checking of input
+
+"""
+from __future__ import annotations
+import logging
+import shutil
+from pathlib import Path
+from pydantic import BaseModel
+
+
+logger = logging.getLogger(__file__)
+
+
+class Model(BaseModel):
+    """Base model class.
+
+    Parameters
+    ----------
+    id: str
+        Model id.
+    kind: str
+        Model kind
+    workdir: str
+        Workspace directory path.
+    children: list[Model]
+        Model child nests
+
+    """
+    id: str
+    kind: str
+    workdir: Path
+    children: list[Model] = []
+    loglevel: str = "INFO"
+
+    def _set_workspace(self):
+        """Create workspace environment."""
+        logger.info(f"Creating workspace directory: {self.workdir}")
+        self.workdir.mkdir(exist_ok=True, parents=True)
+
+    def _del_workspace(self):
+        """Delete workspace environment."""
+        logger.warning(f"Deleting workspace directory: {self.workdir}")
+        shutil.rmtree(self.workdir)
+
+    def _fetch_data(self):
+        """Fetch all forcing data."""
+        pass
+
+    def _write_cmd(self):
+        """Write model command file."""
+        pass
+
+    def preprocess(self):
+        """Create workspace and all files to run the model."""
+        self._set_workspace()
+        self._fetch_data()
+        self._write_cmd()
+
+    def run(self):
+        """Execute model binary."""
+        pass
+
+    def postprocess(self):
+        """Postprocess and upload model results."""
+
