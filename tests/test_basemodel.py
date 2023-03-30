8427d4c370d39ec0532d3fbfc0c1cdb75329beb7
Tom Durrant <t.durrant@oceanum.science>
2023-03-31 10:11:26 +1100

    Fixed tests when not run in tests directory

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -1,16 +1,27 @@
+import os
+
 import pytest
 from utils import compare_files
 
 from rompy.core import BaseModel
 from rompy.templates.base.model import Template
 
+here = os.path.dirname(os.path.abspath(__file__))
+
 
 @pytest.fixture
 def model():
-    return BaseModel(run_id="test_base", output_dir="simulations", template=Template())
+    return BaseModel(
+        run_id="test_base",
+        output_dir=os.path.join(here, "simulations"),
+        template=Template(),
+    )
 
 
 # test generate method
 def test_generate(model):
     model.generate()
-    compare_files("simulations/test_base/INPUT", "simulations/test_base_ref/INPUT")
+    compare_files(
+        os.path.join(here, "simulations/test_base/INPUT"),
+        os.path.join(here, "simulations/test_base_ref/INPUT"),
+    )
