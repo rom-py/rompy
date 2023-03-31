c8b0f4eb1ac0a71616133ca230004804d691fbec
Tom Durrant <t.durrant@oceanum.science>
2023-03-31 16:18:28 +1100

    Refactored generation to be a template method rather than a model one

--- rompy/templates/base/model.py
+++ rompy/templates/base/model.py
@@ -1,9 +1,27 @@
 from __future__ import annotations
 
+import inspect
+import json
+import logging
+import os
+import platform
 from datetime import datetime
 from typing import Optional
 
+import cookiecutter.config as cc_config
+import cookiecutter.generate as cc_generate
+import cookiecutter.repository as cc_repository
 from pydantic import BaseModel, PrivateAttr, validator
+
+logger = logging.getLogger(__name__)
+
+
+def json_serial(obj, datetimefmt="%Y%m%d.%H%M%S"):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        return obj.strftime(datetimefmt)
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
 class Template(BaseModel):
@@ -13,8 +31,10 @@ class Template(BaseModel):
     compute_interval: str = "0.25 HR"
     # compute_stop: datetime = "20200224.040000"
     compute_stop: datetime = datetime(2020, 2, 24, 4)
-    _template: str = "../templates/base"
-    _datafmt: str = "%Y%m%d.%H%M%S"
+    output_dir: str = "template_output"
+    checkout: str | None = None
+    _template: str = ""
+    _datefmt: str = "%Y%m%d.%H%M%S"
     _generated_at: str = PrivateAttr()
     _generated_on: str = PrivateAttr()
     _generated_by: str = PrivateAttr()
@@ -40,3 +60,55 @@ class Template(BaseModel):
             except ValueError:
                 pass
         return v
+
+    def _write_template_json(self) -> str:
+        """Write the cookiecutter.json file from pydantic template"""
+
+        template = os.path.dirname(inspect.getmodule(self).__file__)
+
+        def json_serial_local(obj):
+            return json_serial(obj, self._datefmt)
+
+        ccjson = os.path.join(template, "cookiecutter.json")
+        with open(ccjson, "w") as f:
+            d = self.dict()
+            d.update({"_template": template})
+            d.update({"_generated_at": self._generated_at})
+            d.update({"_generated_by": self._generated_by})
+            d.update({"_generated_on": self._generated_on})
+            f.write(json.dumps(d, default=json_serial_local, indent=4))
+        return template
+
+    def generate(self, output_dir: str = None) -> str:
+        self._generated_at = str(datetime.utcnow())
+        self._generated_by = os.environ.get("USER")
+        self._generated_on = platform.node()
+
+        config_dict = cc_config.get_user_config(
+            config_file=None,
+            default_config=False,
+        )
+
+        template = self._write_template_json()
+
+        repo_dir, cleanup = cc_repository.determine_repo_dir(
+            template=template,
+            abbreviations=config_dict["abbreviations"],
+            clone_to_dir=config_dict["cookiecutters_dir"],
+            checkout=self.checkout,
+            no_input=True,
+        )
+
+        # regenerate the context so that is is correctly templated
+        context = cc_generate.generate_context(
+            context_file=os.path.join(repo_dir, "cookiecutter.json"),
+        )
+
+        staging_dir = cc_generate.generate_files(
+            repo_dir=repo_dir,
+            context=context,
+            overwrite_if_exists=True,
+            output_dir=self.output_dir,
+        )
+        logger.info(f"Successfully generated project in {output_dir}")
+        return staging_dir
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,9 +1,21 @@
-from datetime import datetime, tzinfo
+import os
+from datetime import datetime
 
 import pytest
-from dateutil import tz
+from utils import compare_files
 
 from rompy.templates.base.model import Template
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.fixture
+def template():
+    return Template(
+        run_id="test_base",
+        _template="../rompy/templates/base",
+        output_dir=os.path.join(here, "template_output"),
+    )
 
 
 # write tests for Template class
@@ -13,7 +25,7 @@ def test_template():
     assert template.compute_start == datetime(2020, 2, 21, 4)
     assert template.compute_interval == "0.25 HR"
     assert template.compute_stop == datetime(2020, 2, 24, 4)
-    assert template._template == "../templates/base"
+    assert template._template == ""
 
 
 def test_datetime_parse():
@@ -29,3 +41,26 @@ def test_datetime_parse():
             assert getattr(template.compute_stop, period) == getattr(
                 compute_stop, period
             )
+
+
+def test_datetime_parse_fail():
+    compute_stop = datetime(2022, 2, 21, 4)
+    for format in [
+        "%Y%m%d.%Hhello",
+        "%Y%m%dhello",
+    ]:
+        try:
+            template = Template(compute_stop=compute_stop.strftime(format))
+        except ValueError:
+            pass
+        else:
+            raise ValueError("Should not be able to parse {format}")
+
+
+# test generate
+def test_generate(template):
+    template.generate()
+    compare_files(
+        os.path.join(here, "template_output/test_base/INPUT"),
+        os.path.join(here, "simulations/test_base_ref/INPUT"),
+    )
