1a28ba83674b3c940a1a8a1790bea48a17d32956
Tom Durrant <t.durrant@oceanum.science>
2023-04-21 11:47:06 +1200

    Added missing init file from core

---
+++ rompy/core/__init__.py
@@ -0,0 +1,7 @@
+from .config import BaseConfig
+from .data import DataBlob, DataGrid
+from .filters import *
+from .grid import BaseGrid, RegularGrid
+from .model import BaseModel
+from .time import DateTimeRange
+from .types import *
