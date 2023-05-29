31c5e8dda118f819fe48523018ae08673d6df48a
Tom Durrant <t.durrant@oceanum.science>
2023-05-29 23:53:33 +1000

    Moved template to config argument instead of modelrun

--- rompy/core/model.py
+++ rompy/core/model.py
@@ -30,14 +30,6 @@ class ModelRun(RompyBaseModel):
     )
     output_dir: str = Field("simulations", description="The output directory")
     config: BaseConfig = Field(BaseConfig(), description="The configuration object")
-    template: Optional[str] = Field(
-        description="The path to the model template",
-        default="/source/rompy/rompy/templates/base",
-    )
-    checkout: Optional[str] = Field(
-        description="The git branch to use",
-        default="main",
-    )
     _datefmt: str = "%Y%m%d.%H%M%S"
 
     @property
@@ -72,7 +64,7 @@ class ModelRun(RompyBaseModel):
         logger.info("-----------------------------------------------------")
         logger.info("Model settings:")
         logger.info(self)
-        logger.info(f"Template used to generate model: {self.template}")
+        logger.info(f"Template used to generate model: {self.config.template}")
 
         cc_full = {}
         cc_full["runtime"] = self.dict()
@@ -86,15 +78,14 @@ class ModelRun(RompyBaseModel):
         else:
             cc_full["config"] = self.config
 
-        staging_dir = render(cc_full, self.template, self.output_dir, self.checkout)
+        staging_dir = render(
+            cc_full, self.config.template, self.output_dir, self.config.checkout
+        )
 
         logger.info("")
         logger.info(f"Successfully generated project in {self.output_dir}")
         logger.info("-----------------------------------------------------")
         return staging_dir
-
-    def write(self):
-        self.config.write(self)
 
     def zip(self) -> str:
         """Zip the input files for the model run
