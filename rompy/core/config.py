27222190de3563dd74fd55dd8c95b0764bd3ea34
Tom Durrant <t.durrant@oceanum.science>
2023-05-30 22:27:52 +1000

    Revert "Moved template to config argument instead of modelrun"
    This reverts commit 31c5e8dda118f819fe48523018ae08673d6df48a.

--- rompy/core/config.py
+++ rompy/core/config.py
@@ -1,15 +1,10 @@
 import logging
-import os
-from typing import Optional
 
-from pydantic import Field
 from typing_extensions import Literal
 
 from .types import RompyBaseModel
 
 logger = logging.getLogger(__name__)
-
-here = os.path.dirname(os.path.abspath(__file__))
 
 
 class BaseConfig(RompyBaseModel):
@@ -24,15 +19,6 @@ class BaseConfig(RompyBaseModel):
     """
 
     model_type: Literal["base"] = "base"
-    template: Optional[str] = Field(
-        description="The path to the model template",
-        default="/source/rompy/rompy/templates/base",
-        # default=os.path.join(here, "../rompy/templates/swan"),
-    )
-    checkout: Optional[str] = Field(
-        description="The git branch to use",
-        default="main",
-    )
 
     class Config:
         extra = "allow"
