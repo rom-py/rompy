14c31b731877b2244ef80d3ad89c46e235a0b15b
Rafael Guedes <r.guedes@oceanum.science>
2023-06-06 12:28:26 +1200

    Stop hardcoding default template path

--- rompy/core/config.py
+++ rompy/core/config.py
@@ -1,12 +1,16 @@
 import logging
 from typing import Optional
+from pathlib import Path
 
 from pydantic import Field
 from typing_extensions import Literal
 
 from .types import RompyBaseModel
 
+
 logger = logging.getLogger(__name__)
+
+DEFAULT_TEMPLATE = str(Path(__file__).parent.parent / "templates/base")
 
 
 class BaseConfig(RompyBaseModel):
@@ -15,7 +19,7 @@ class BaseConfig(RompyBaseModel):
     model_type: Literal["base"] = "base"
     template: Optional[str] = Field(
         description="The path to the model template",
-        default="/source/rompy/rompy/templates/base",
+        default=DEFAULT_TEMPLATE,
     )
     checkout: Optional[str] = Field(
         description="The git branch to use",
