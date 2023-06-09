3016149bad8cd69ae84437616581f542ddbe39f9
Tom Durrant <t.durrant@oceanum.science>
2023-06-10 08:55:10 +1000

    WIP - reconciling grids between forcing and boundary data

--- rompy/core/__init__.py
+++ rompy/core/__init__.py
@@ -1,5 +1,5 @@
 from .config import BaseConfig
-from .data import DataBlob, DataGrid
+from .data import DataBlob, DataGrid, Dataset, DatasetIntake, DatasetXarray
 from .filters import *
 from .grid import BaseGrid, RegularGrid
 from .model import ModelRun
