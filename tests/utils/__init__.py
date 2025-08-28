8dad40793065f3dd0b3ced3f3ee37d1ee4b7eff0
Tom Durrant <t.durrant@oceanum.science>
2025-08-28 14:17:22 +1000

    Cleaned up removed schism util from init

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -4,11 +4,12 @@ Testing utilities package.
 This package provides common utilities used by tests across different modules.
 """
 
-from pathlib import Path
 import sys
+from pathlib import Path
 
 # Import utilities from modules
-from .compare import compare_files, compare_nmls, compare_nmls_values, untar_file
+from .compare import (compare_files, compare_nmls, compare_nmls_values,
+                      untar_file)
 from .schism import download_hycom
 
 # Export the utility functions
@@ -18,6 +19,4 @@ __all__ = [
     "compare_nmls",
     "compare_nmls_values",
     "untar_file",
-    # SCHISM-specific utilities
-    "download_hycom",
 ]
