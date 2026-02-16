a2405cf72fa7a551c22f482ee1389258de3243a2
Tom Durrant <t.durrant@oceanum.science>
2026-02-16 11:52:41 +1100

    test(datablob): add HTTP test infrastructure

---
+++ tests/test_datablob_http.py
@@ -0,0 +1,63 @@
+import pytest
+import httpx
+import respx
+
+
+def test_http_download_success():
+    pytest.fail("RED: test_http_download_success not implemented")
+
+
+def test_http_404_no_retry():
+    pytest.fail("RED: test_http_404_no_retry not implemented")
+
+
+def test_http_503_retries():
+    pytest.fail("RED: test_http_503_retries not implemented")
+
+
+def test_http_retry_success():
+    pytest.fail("RED: test_http_retry_success not implemented")
+
+
+def test_http_size_limit():
+    pytest.fail("RED: test_http_size_limit not implemented")
+
+
+def test_datablob_accepts_http_url():
+    pytest.fail("RED: test_datablob_accepts_http_url not implemented")
+
+
+def test_datablob_http_link_error():
+    pytest.fail("RED: test_datablob_http_link_error not implemented")
+
+
+def test_datablob_http_get():
+    pytest.fail("RED: test_datablob_http_get not implemented")
+
+
+def test_datablob_http_cache():
+    pytest.fail("RED: test_datablob_http_cache not implemented")
+
+
+def test_http_url_with_query_params():
+    pytest.fail("RED: test_http_url_with_query_params not implemented")
+
+
+def test_http_url_with_encoding():
+    pytest.fail("RED: test_http_url_with_encoding not implemented")
+
+
+def test_http_url_no_filename():
+    pytest.fail("RED: test_http_url_no_filename not implemented")
+
+
+def test_http_redirect_handling():
+    pytest.fail("RED: test_http_redirect_handling not implemented")
+
+
+def test_http_insecure_warning():
+    pytest.fail("RED: test_http_insecure_warning not implemented")
+
+
+def test_http_timeout():
+    pytest.fail("RED: test_http_timeout not implemented")
